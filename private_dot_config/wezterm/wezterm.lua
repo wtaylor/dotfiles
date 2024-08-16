7dc970eb54aaac275fe6068ea0fe76d2984df98c
William Taylor <me@willtaylor.info>
2024-08-16 12:55:23 +0100

    regular update

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,13 +1,13 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = 'Nord (base16)'
-config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.color_scheme = "Nord (base16)"
+-- config.font = wezterm.font 'FiraCode Nerd Font Mono'
 
 -- and finally, return the configuration to wezterm
 return config
