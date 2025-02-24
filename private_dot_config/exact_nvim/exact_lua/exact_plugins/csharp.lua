c9596230f918eb25322de88729ba70ed96672680
William Taylor <me@willtaylor.info>
2025-02-24 19:13:54 +0000

    fix(nvim): add csharp debugging configuration

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/csharp.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/csharp.lua
@@ -16,18 +16,50 @@ return {
       formatters_by_ft = {
         cs = { "csharpier" },
       },
-      -- formatters = {
-      --   csharpier = {
-      --     command = "dotnet-csharpier",
-      --     args = { "--write-stdout" },
-      --   },
-      -- },
     },
   },
   {
     "mfussenegger/nvim-dap",
     opts = function()
       local dap = require("dap")
+      local dotnet_build_project = function()
+        local default_path = vim.fn.getcwd() .. "/"
+        if vim.g["dotnet_last_proj_path"] ~= nil then
+          default_path = vim.g["dotnet_last_proj_path"]
+        end
+        local path = vim.fn.input("Path to your *proj file", default_path, "file")
+        vim.g["dotnet_last_proj_path"] = path
+        local cmd = "dotnet build -c Debug " .. path .. " > /dev/null"
+        print("")
+        print("Cmd to execute: " .. cmd)
+        local f = os.execute(cmd)
+        if f == 0 then
+          print("\nBuild: ✔️ ")
+        else
+          print("\nBuild: ❌ (code: " .. f .. ")")
+        end
+      end
+
+      local dotnet_get_dll_path = function()
+        return coroutine.create(function(dap_run_co)
+          local items = vim.fn.globpath(vim.fn.getcwd(), "**/bin/Debug/**/*.dll", 0, 1)
+          local opts = {
+            format_item = function(path)
+              return vim.fn.fnamemodify(path, ":t")
+            end,
+          }
+          local function cont(choice)
+            if choice == nil then
+              return nil
+            else
+              coroutine.resume(dap_run_co, choice)
+            end
+          end
+
+          vim.ui.select(items, opts, cont)
+        end)
+      end
+
       if not dap.adapters["netcoredbg"] then
         require("dap").adapters["netcoredbg"] = {
           type = "executable",
@@ -47,7 +79,10 @@ return {
               request = "launch",
               ---@diagnostic disable-next-line: redundant-parameter
               program = function()
-                return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
+                if vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1 then
+                  dotnet_build_project()
+                end
+                return dotnet_get_dll_path()
               end,
               cwd = "${workspaceFolder}",
             },
