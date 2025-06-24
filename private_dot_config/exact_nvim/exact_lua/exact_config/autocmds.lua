d1542c4143730e38042d7fd35f961228b61a8801
William Taylor <me@willtaylor.info>
2025-06-24 18:09:18 +0100

    fix(nvim): godot autocmd start nvim server

--- private_dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Start server when cwd contains godot project
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("vvim", { clear = true }),
+  callback = function(event)
+    if vim.fs.dirname(vim.fs.find({ "project.godot" }, { limit = 10 })[1]) ~= nil then
+      vim.api.nvim_command('echo serverstart("/tmp/nvim-godot.pipe")')
+    end
+  end,
+})
